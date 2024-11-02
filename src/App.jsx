e835a0366c308fffe8f9579b07dd7162bd71f022
Hashmics <rehmanhashmirehman123@gmail.com>
2024-11-03 00:45:00 +0500

    Companies section completed

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import Header from "./components/Header/Header"
 import Hero from "./components/Hero/Hero"
 import './App.css'
+import Companies from "./components/companies/Companies"
 
 
 function App() {
@@ -11,6 +12,7 @@ function App() {
         <Header />
         <Hero />
       </div>
+      <Companies />
     </div >
   )
 }
